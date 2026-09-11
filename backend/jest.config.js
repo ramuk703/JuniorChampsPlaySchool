4a55e4f1d4775073d5ee4eb5b7ad8ce951ccedc5
ramuk703 <kumarmukesh14530@gmail.com>
2026-09-11 08:26:41 -0400

    Security: add authentication regression tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: "node",
   clearMocks: true,
   restoreMocks: true,
+  testMatch: [
+    "<rootDir>/tests/security.test.js"
+  ],
   moduleNameMapper: {
     "^uuid$": "<rootDir>/tests/mocks/uuid.js"
   }
